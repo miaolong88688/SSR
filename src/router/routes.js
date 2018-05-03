6ce7821bbfa5d56e4e2e7afd76dafa55b3c9febb
miaolong888 <miaolong88688@163.com>
2018-05-03 15:06:17 +0800

    initSSR

---
+++ src/router/routes.js
@@ -0,0 +1,19 @@
+
+const Home = () => import(/* webpackChunkName: "home" */'@/views/home/index.vue');
+
+const routes = [
+    {
+        path: '/home',
+        meta: {
+            title: 'home',
+            index: 0
+        },
+        component: Home
+    },
+    {
+        path: '*',
+        redirect: '/home'
+    }
+]
+
+export default routes;
